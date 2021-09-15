5b64d9061ab9a44361873934af5c5895a67425c6
Atharva7128 <Atharva.Nadkarni@lntinfotech.com>
2021-09-15 17:41:53 +0530

    loas

---
+++ LTIMiniProject/src/Source/Welcome.java
@@ -0,0 +1,12 @@
+package Source;
+
+public class Welcome {
+	public static void main(String[] args) {
+
+
+		System.out.println("Namaste");
+		System.out.println("hope you are doing well");
+		System.out.println("Hi my name is Riya");
+		System.out.println("Welcome");
+	}
+}
